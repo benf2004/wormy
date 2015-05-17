dfa7c89597b5b6025aabaeaa182c78e6f2e78a15
Greg Finch <finchgregoryc@johndeere.com>
2015-05-16 21:31:24 -0500

    food truck

--- wormy/wormy/FoodTruck.swift
+++ wormy/wormy/FoodTruck.swift
@@ -9,14 +9,36 @@
 import Foundation
 
 struct FoodTruck {
+    var foodTypes : Array<String> = []
+    var frequencies : Array<Int> = []
+    
     init(properties : NSDictionary) {
         if let foodTruck = properties["FoodTruck"] as? NSDictionary {
-            for food in foodTruck {
-//                println(food)
-//                if let item = foodTruck[food] as? String {
-//                    println(item)
-//                }
+            for (food, frequency) in foodTruck {
+                foodTypes.append(food as! String)
+                frequencies.append(frequency as! Int)
             }
+        }
+    }
+    
+    func randomFoodType() -> String {
+        var random = Game.randomInRange(0, hi: 99)
+        var total = 0
+        for (i, val) in enumerate(frequencies) {
+            total = total + val
+            if random < total {
+                return foodTypes[i]
+            }
+        }
+        return foodTypes.last!
+    }
+    
+    func randomFood() -> BaseWorm {
+        switch randomFoodType() {
+        case "GravityWorm":
+            return GravityWorm(textureName: Textures.simple)
+        default:
+            return BaseWorm(textureName: Textures.simple)
         }
     }
 }
